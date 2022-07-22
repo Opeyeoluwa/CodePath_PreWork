2b2a3fd31974735981c3d0c656253f0f829d171d
Opeyeoluwa Olanipekun <olanipekunopeyeoluwa1@gmail.com>
2022-07-21 19:21:56 -0700

    Pre-Work uplaod

---
+++ OpeHelloWorld/app/src/main/java/com/example/opehelloworld/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.opehelloworld
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.RelativeLayout
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.button).setOnClickListener()
+        {
+           Log.i( "Ope", "Tapped on Button" )
+            findViewById<TextView>(R.id.textView).setTextColor(getResources().getColor(R.color.white))
+        }
+    }
+}
